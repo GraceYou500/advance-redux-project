8c355fe41c65f30d7092bcb8179f7241dc2934a9
Grace You <graceyouya500@gmail.com>
2022-08-20 17:23:32 +1000

    remove cart

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux/es/exports';
-import uiSlice from './store/ui-slice';
 
 function App() {
-  const showCart = useSelector(state => state.ui.showCart);
+  const showCart = useSelector(state => state.ui.showCart); // the function will receive redux state(can think as the store) auto, and will execute by react-redux.
 
   return (
     <Layout>
---
+++ src/store/cart-slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCartState = { items: [], totalQuantity: 0, totalAmount: 0 };
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(item => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart() {},
+  },
+});
